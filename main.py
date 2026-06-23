1a85c47c6f8a599affb4720c6802ad6fd68e922f
JvSchulz <vitorsm66@hotmail.com>
2026-06-23 18:41:10 -0300

    small improvements

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from src.gateways.watson import watson_gateway
+from src.gateways.watson.watson_gateway import WatsonGateway
 
 app = FastAPI()
 
+watson_gateway = WatsonGateway()
 watson_gateway.test_assistant()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World"} 
